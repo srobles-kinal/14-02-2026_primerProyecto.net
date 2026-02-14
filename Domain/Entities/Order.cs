728cbcb4d0151ed2c1dfaf47dcf59c7e4026e98d
Sergio Robles <srobles-i001r@kinal.edu.gt>
2026-02-14 09:36:00 -0600

    Initial commit

---
+++ Domain/Entities/Order.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkerOrderManagement.Domain.Entities
+{
+    public class Order
+    {
+        
+    }
+}
